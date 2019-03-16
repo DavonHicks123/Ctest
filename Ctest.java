04dca4980e828758153529625b2b6d55b79e12f3
DavonHicks123 <47585618+DavonHicks123@users.noreply.github.com>
2019-03-15 23:35:41 -0400

    Add files via upload

---
+++ Ctest.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Ctest {
+
+	public static void main(String[] args) {
+		ArrayList taxRates = new ArrayList();
+		taxRates.add(0.10);
+		taxRates.add(0.15);
+		taxRates.add(0.21);
+		taxRates.add(0.28);
+		taxRates.add(0.31);
+
+	}
+
+}
